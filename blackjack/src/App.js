8ee93c25dd0632cb44eb1cdb01c8a1a85114b70d
malteronie <titouliden83@hotmail.com>
2024-12-20 07:45:05 +0100

    start

---
+++ blackjack/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './Card.jsx'
+import CardList from './Card.jsx';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>BlackJack</h1>
+        <CardList/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
