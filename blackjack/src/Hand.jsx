db796b31814213dc2c13c43a24b7561868f8c96b
malteronie <153739321+malteronie@users.noreply.github.com>
2025-01-07 09:00:49 +0100

    maj

--- blackjack/src/Hand.jsx
+++ blackjack/src/Hand.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import Card from "./Card";
-import "./App.css";
 
 const Hand = () => {
-  const cards = [
-    { value: "A", type: "♠" },
-    { value: "10", type: "♦" },
+  const types = ["♠", "♥", "♦", "♣"];
+  const values = [
+    "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A",
   ];
 
+  // Fonction pour tirer une carte au hasard
+  const getRandomCard = () => {
+    const type = types[Math.floor(Math.random() * types.length)];
+    const value = values[Math.floor(Math.random() * values.length)];
+    return { value, type };
+  };
+
+  // Génération de deux cartes
+  var cards = [getRandomCard(), getRandomCard()];
+
+  const takeCard = () => {
+    cards = [ cards, getRandomCard]
+  }
   return (
     <div className="hand">
+
+      <button onClick={takeCard}>Prendre une carte</button>
       <h2>Your Hand</h2>
       <div className="cards">
         {cards.map((card, index) => (
